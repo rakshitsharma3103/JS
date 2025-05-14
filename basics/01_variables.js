cfcc7a1427cf8ba363e5002fc8963d18c99d75e7
Rakshit Sharma <134766414+rakshitsharma3103@users.noreply.github.com>
2025-05-14 11:41:57 +0000

    Variables in JS

--- basics/01_variables.js
+++ basics/01_variables.js
@@ -2,7 +2,7 @@
 const accountId = 144553
 let accountEmail = "rakshit@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"   // It is Possible to write without keyword but not a good approach.
+accountCity = "Jaipur"   // It is Possible to write without keyword but not a good approach.JavaScript will still allow it by making it a global variable, but it can lead to bugs and confusion.
 let accountState;
 
 // accountId = 2 // not allowed
@@ -16,7 +16,10 @@ console.log(accountId);
 
 /*
 Prefer not to use var
-because of issue in block scope and functional scope
+because of issue in block scope and functional scope.
+
+var is not block-scoped, so it can leak outside {} blocks.
+let and const are block-scoped, which is safer.
 */
 
 
