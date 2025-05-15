4c34e4e8a4e9dca2be0b1cb1fc8886c0ca1f12e3
Rakshit Sharma <134766414+rakshitsharma3103@users.noreply.github.com>
2025-05-15 09:41:41 +0000

    Arrays_2

---
+++ basics2/02_arrays.js
@@ -0,0 +1,31 @@
+const marvel_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)          // push directly makes change in original array
+
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)          // Concat do not change original array it need to store in new place.
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]       // This is called "Spread"
+
+// console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) // here in place of (Infinity) we can give depth upto which we have to check array.
+console.log(real_another_array);
+
+
+
+console.log(Array.isArray("Rakshit"))   // check isArray or not
+console.log(Array.from("Rakshit"))      // convert in Array.
+console.log(Array.from({name: "rakshit"})) // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
