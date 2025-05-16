416f8e729d5d9a84c538d91b45debf37db021fa9
Rakshit Sharma <134766414+rakshitsharma3103@users.noreply.github.com>
2025-05-16 16:36:45 +0000

    Objects in JS

---
+++ basics2/03_objects.js
@@ -0,0 +1,41 @@
+/* singleton => "A singleton ensures that only one object is created from a class." 
+                    OR
+"A singleton is a design pattern where only one object is created from a class (constructor), ensuring that the class has only one instance."
+*/
+// Object.create is the method to create object with constructor
+
+// object literals
+
+const mySym = Symbol("key1")          // Symbol is defined here
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",    // Symbol used as key here
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)     // After freezing object no change propagate .
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
