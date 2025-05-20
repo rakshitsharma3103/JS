0ed38849c25cbb7a8234651ef2d366bf6650c04d
Rakshit Sharma <134766414+rakshitsharma3103@users.noreply.github.com>
2025-05-20 17:47:55 +0000

    forin loop discussion

---
+++ iterations/four.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {              // forin loop can access or iterate on objects
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {             // Map are not iteratable by forin loop
+//     console.log(key);
+// }
