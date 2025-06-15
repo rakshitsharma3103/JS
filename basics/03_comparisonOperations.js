1cc93920bff0c0c42cc6e833d465d33a1669e41d
Rakshit Sharma <134766414+rakshitsharma3103@users.noreply.github.com>
2025-06-15 12:19:40 +0000

    comparison Operations

--- basics/03_comparisonOperations.js
+++ basics/03_comparisonOperations.js
@@ -36,7 +36,7 @@ let negValue = -value
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
+// console.log(2**3);          // 8
 // console.log(2/3);
 // console.log(2%3);
 
@@ -44,12 +44,12 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+ // console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);       //12
+// console.log(1 + "2");       //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2");   //32
 
 // console.log( (3 + 4) * 5 % 3);
 
