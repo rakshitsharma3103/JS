b0b75288ed7a9aff43c1e254dd30a5c8b350211c
Rakshit Sharma <134766414+rakshitsharma3103@users.noreply.github.com>
2025-06-23 01:27:33 +0000

    dates in js

--- basics/07_datesinJS.js
+++ basics/07_datesinJS.js
@@ -1,16 +1,17 @@
 // Dates
 
-let myDate = new Date()
- console.log(myDate.toString());
- console.log(myDate.toDateString());
- console.log(myDate.toLocaleString());
+let myDate = new Date()     //Here we are creating an object using the Date constructor, and this object inherits from Date.prototype, so it can access all prototype methods.
+ 
+ console.log(myDate.toString());    //Returns a full, human-readable string representing the date with time and time zone.
+ console.log(myDate.toDateString());   //Returns a simplified version of the date without the time.
+ console.log(myDate.toLocaleString());      //Returns the date and time formatted according to your local settings
 // console.log(typeof myDate);       // Date is object type in JS
 
 // let myCreatedDate = new Date(2023, 0, 23)    // Month start with 0 in js
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)  //January 23, 2023 at 05:03 AM
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
 
@@ -20,6 +21,7 @@ let myTimeStamp = Date.now()
 
 let newDate = new Date()
 console.log(newDate);
+console.log(newDate.toString());
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
 
