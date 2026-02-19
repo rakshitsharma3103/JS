0611b392d3614cec97f8d0d196e56a3daa8a6926
Rakshit Sharma <134766414+rakshitsharma3103@users.noreply.github.com>
2026-02-19 15:56:18 +0530

    Some comments added
    Added explanation for comparison behavior of null and undefined.

--- basics/04_comparison.js
+++ basics/04_comparison.js
@@ -9,8 +9,10 @@
 // console.log("02" > 1);   // true
 
 console.log(null > 0);  //false
-console.log(null == 0);    // false
+console.log(null == 0);    // false     
 console.log(null >= 0);     // true
+            // The reason is that an equality check(==) and comparisons >,<,>=,<= work differently is-
+            // Because Comparisons convert null to a number, treating it as 0.
 
 console.log(undefined == 0); //false
 console.log(undefined > 0);  //false
